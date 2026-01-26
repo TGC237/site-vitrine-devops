2978c5d319360a30345dd3b9feb3352979160177
root <root@srv1069524.hstgr.cloud>
2026-01-26 00:40:43 +0000

    Feature: Ajout des routes et pages Architecture/Documentation

--- src/main/java/com/tgc/vitrine/VitrineApplication.java
+++ src/main/java/com/tgc/vitrine/VitrineApplication.java
@@ -12,9 +12,20 @@ public class VitrineApplication {
     public static void main(String[] args) {
         SpringApplication.run(VitrineApplication.class, args);
     }
-
+	// Route de la page acceuil index
     @GetMapping("/")
     public String index() {
         return "index"; // Cela va chercher le fichier src/main/resources/templates/index.html
+    }
+	// Route pour la page Architecture
+    @GetMapping("/architecture")
+    public String architecture() {
+        return "architecture"; // Cherche le fichier architecture.html
+    }
+
+    	// Route pour la page Documentation
+    @GetMapping("/documentation")
+    public String documentation() {
+        return "documentation"; // Cherche le fichier documentation.html
     }
 }
